4802dc02d389d7b04fc500bc0d9b8733fbc301f8
sgrodriguez <sgrodriguez@dc.uba.ar>
2017-01-28 20:23:05 -0300

    catch sigint signal and saves all the data

--- how_many.py
+++ how_many.py
@@ -2,6 +2,7 @@
 import json
 import signal
 import sys
+import os
 
 # Non Standard imports
 import requests
@@ -15,22 +16,27 @@ class Dictionary:
 		self.top5 = []
 		self.cantPalabras = 0
 
+	def crear(self, wordDictionary, topList):
+		self.dicc = wordDictionary
+		self.top5 = topList
+		self.cantPalabras = len(wordDictionary.keys())
+
 	def agrego_palabra(self,palabra):
-		# Transformo todo a minuscula
+		""" Parsing to lower letters """
 		palabra = palabra.lower()
 		if palabra in self.dicc:
 			self.dicc[palabra] = self.dicc[palabra] + 1
 		else:
 			self.dicc[palabra] = 1
 			self.cantPalabras = self.cantPalabras + 1
-		# si ya esta en el top 5 actualizo
+		""" If it is in top5 update the count """
 		apariciones = self.dicc[palabra]
 		if (palabra,apariciones-1) in self.top5:
 			indice = self.top5.index((palabra,apariciones-1))
 			self.top5[indice] = (palabra,apariciones)
 		else:
 			self.top5.append((palabra,apariciones))
-		#ordeno la lista y me quedo con las 5 primeras		
+		""" Sort the list and keep the first 5 words """	
 		self.top5.sort(key=lambda tup: tup[1],reverse=True)
 		#keep the 5
 		if len(self.top5) > 5:
@@ -48,26 +54,54 @@ class Dictionary:
 	def cantidad_palabras_diferentes(self):
 		return self.cantPalabras	
 
-# Look up for the token in token.in file
+	def backup(self,chatid):
+		 with open(os.path.join("backups", str(chatid)), 'w') as file:
+		 	for key in self.dicc.keys():
+		 		file.write(key+' '+ str(self.dicc[key])+"\n")
+		 	file.write("top"+"\n")
+		 	for top in self.top5:
+		 		file.write(top[0]+' '+str(top[1])+"\n")
+
+""" Look up for the token in token.in file """
 try: 
 	with open('token.in','r') as file:
 		token = file.readline().rstrip('\n')
 except IOError:
 	print "No encontre el archivo token.in"
 else:
-	""" Si no encuentra el token nunca define TELEGRAM_URL y se cierra """
+	""" if not find the token.in never define TELEGRAM_URL and close """
 	TELEGRAM_URL = "https://api.telegram.org/bot" + token
 
-# Conjunto de palabras a ignorar
+""" set of words to ignore """
 
 ignaradas = set(['y','el','la','los','yo','mi','mis','ellos','ellas','nosotros',',','.','que',' ',"\n"])
 
+
+"""Dicctionary to save for every chat the data"""
+dicc_chat = {}
+
 def main():
+	""" Look up for previous backups """
+	if os.path.exists("backups"):
+		for filename in os.listdir("backups"):
+			 with open(os.path.join("backups", filename), 'r') as fread:
+			 	dic_temporal = {}
+			 	list_top_temporal = []
+			 	noEsTop = True
+			 	for line in fread.read().splitlines():
+			 		if noEsTop and line != "top":
+			 			dic_temporal[line.split(' ')[0]] = line.split(' ')[1]
+			 		elif line == "top":
+			 			noEsTop = False		
+					else:
+						list_top_temporal.append((line.split(' ')[0],line.split(' ')[1]))
+				dicc_chat[int(filename)] = Dictionary()
+				dicc_chat[int(filename)].crear(dic_temporal,list_top_temporal)
+
+
 	r = requests.get(TELEGRAM_URL + "/getUpdates",params={'timeout': 0})
 	last_update_id = 0
 	validjson = True
-	"""Diccionario donde guardo para cada chat su historial de palabras"""
-	dicc_chat = {}
 
 	try:
 		data = json.loads(r.text)
@@ -103,7 +137,7 @@ def main():
 	            if 'message' in result and 'text' in result['message']:
 	            	message = result['message']
 	                chat_id = message['chat']['id']
-	                # si es la primera vez que le escriben al bot agrega el chat al dicc de chats
+	                """If it is the first time that the bot register a word from a chat it join to dicc_chat """
 	                print chat_id
 	                if chat_id not in dicc_chat:
 	                	dicc_chat[chat_id] = Dictionary() 
@@ -135,7 +169,7 @@ def main():
 	            # Timeout, nada que hacer
 	            pass
 
-""" Handlers de los comandos"""
+""" Handlers """
 
 def bot_send_msg(chat_id, text):
 	"""Realiza un POST al chat_id indicado con el mensaje text."""
@@ -159,7 +193,7 @@ def how_many(dict,message):
 	text = message['text']
 	text_split = text.split(' ')
 	if len(text_split) > 1:
-		#Pasamos la query a minuscula
+		#parsing the query to lower
 		query = text_split[1].lower()
 		apariciones = dict.how_many(text_split[1])
 		msj = "La palabra "+query+" se uso "+ str(apariciones) +" veces."
@@ -180,6 +214,28 @@ def cantidad_palabras(dict,message):
 	msj = "Se usaron "+ str(cant_palabras) +" palabras diferentes hasta el momento."
 	bot_send_msg(chat_id,msj)
 
+def backup_chats(diccChats):
+	if not diccChats == {}:
+		""" If directory backup dont exist we create it """
+		if not os.path.exists("backups"):
+			os.makedirs("backups")
+		""" For every chat we backup the data """
+		for chat in diccChats.keys():
+			diccChats[chat].backup(chat)
+			print "Backup del chat "+ str(chat) + " finalizado"
+	else:
+		print "Nada que backupear"
+
+def sigint_backup(sign, frame):
+    """Handler of SIGINT and backup data"""
+    print ('Signal: ' + str(sign))
+    print ('Frame : ' + str(frame))
+    print ('Ctrl+C pressed. Exiting.')
+    backup_chats(dicc_chat)
+    sys.exit(0)
+
+# Catch SIGINT using signal_handler
+signal.signal(signal.SIGINT, sigint_backup)
 
 if __name__ == '__main__':
     main()
